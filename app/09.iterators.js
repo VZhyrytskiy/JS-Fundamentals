469b75ae91650421ae05b7f2c2782cbe64985feb
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2020-01-08 11:41:38 +0200

    add new task to iterators

--- app/09.iterators.js
+++ app/09.iterators.js
@@ -8,35 +8,56 @@ console.log('Topic: Iterators');
 //     which takes an objects and returns iterable object.
 //     Iterable object allows you to get key/value pairs.
 //     Display in a console all colors from the object colors.
-const colors = {
-  green: '#0e0',
-  orange: '#f50',
-  pink: '#e07'
-};
+// const colors = {
+//   green: '#0e0',
+//   orange: '#f50',
+//   pink: '#e07'
+// };
 
 // const itColors = keyValueIterable(colors);
 // for (const [, color] of itColors) {
 //   console.log(color);
 // }
 
+
 // Task 2
+// RU: В коллекции хранятся все имена пользователей, которые присоединились к определенной группе телеграмм.
+//     Булевый флаг указывает, является ли пользователь администратором группы.
+//     Создайте итератор, который возвращает только имена администраторов.
+// EN: The following collection store all the names of the user that have joined a particular telegram group. 
+//     The boolean flag indicates whether a user is an administrator of the group.
+//     Сreatereate an iterator that returns only the administrators' names.
+
+// const users = {
+//   anna: false,
+//   boris: true, // admin
+//   christina: false,
+//   dave: false,
+//   elena: false,
+//   felix: true,  // admin
+// };
+// [...users].forEach(name => console.log(name)); // boris, felix 
+
+
+// Task 3
 // RU: Написать функцию take(sequence, amount), которая из бесконечного итерируемого объекта random
 //     вернет указаное количество элементов.
 // EN: Create a function take(sequence, amount), which returns a specified amount of numbers
 //     from iterable object random
 
-const random = {
-  [Symbol.iterator]: () => ({
-    next: () => ({
-      value: Math.random()
-    })
-  })
-};
+// const random = {
+//   [Symbol.iterator]: () => ({
+//     next: () => ({
+//       value: Math.random()
+//     })
+//   })
+// };
 
 // const a = [...take(random, 3)];
 // console.log(a);
 
-// Task 3
+
+// Task 4
 // RU: Написать итерируемый итератор, который возвращает числа Фибоначи
 //     Реализовать метод return для остановки итератора с помощью for-of + break
 // EN: Create iterable iterator, which produces Fibonacci numbers
@@ -47,7 +68,8 @@ const random = {
 //   if (v > 50) break;
 // }
 
-// Task 4
+
+// Task 5
 // RU: Написать итератор для чисел, который позволит получать массивы последовательных целых элементов.
 //     Например, [...-3] => [0, -1, -2, -3], [...3] => [0, 1, 2, 3]
 // EN: Create iterator for numbers, which allows you to get arrays of sequential integers.
