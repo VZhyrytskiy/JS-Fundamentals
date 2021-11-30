0c39d1daa7372da686eebc88233ce81bbf4b22bd
Vertigo <vertigomaill@gmail.com>
2021-11-30 13:06:07 +0300

    updated dependecies

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,9 @@ module.exports = {
   mode: "development",
 	devtool: "inline-source-map",
 	devServer: {
-		contentBase: path.join(__dirname, "dist"),
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
 		port: 3000,
 		open: true,
 		compress: true
