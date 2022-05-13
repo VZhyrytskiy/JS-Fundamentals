c5ec6053b9198c3d2a94d6505ffecce35344a779
Georgy Nikitin <gvnikitin@inno.tech>
2022-05-13 09:57:57 +0300

    Task-2

--- app/interview-task/test.js
+++ app/interview-task/test.js
@@ -1,14 +1,14 @@
-function reverseInPlace(str){
-  console.log( str.split(' ').reverse().join(' ')
-  .split('')
-  .reverse()
-  .join('')
-  );
+// function reverseInPlace(str){
+//   console.log( str.split(' ').reverse().join(' ')
+//   .split('')
+//   .reverse()
+//   .join('')
+//   );
 
-  return str.split(' ').reverse().join(' ').split('').reverse().join('');
-}
+//   return str.split(' ').reverse().join(' ').split('').reverse().join('');
+// }
 
-console.log(reverseInPlace('I am the good boy'));
+// console.log(reverseInPlace('I am the good boy'));
 
 
 // function swapNumb(a, b){
